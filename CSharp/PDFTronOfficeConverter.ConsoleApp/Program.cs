a78ccdfece46ccad68e8f7484f8cec01058f9017
Jelani Jackson <jeljack86@gmail.com>
2020-07-05 12:47:51 -0500

    Created console app to test functionality

--- CSharp/PDFTronOfficeConverter.ConsoleApp/Program.cs
+++ CSharp/PDFTronOfficeConverter.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using pdftron.Filters;
+using System;
+using System.IO;
 
 namespace PDFTronOfficeConverter.ConsoleApp
 {
@@ -6,7 +8,30 @@ namespace PDFTronOfficeConverter.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length < 1)
+            {
+                throw new ArgumentException("Must provide file path of office file");
+            }
+
+            var inputFilePath = args[0];
+
+            if (!File.Exists(inputFilePath))
+            {
+                throw new FileNotFoundException();
+            }
+
+            var fileDirectory = Directory.GetParent(inputFilePath).FullName;
+            var fileWithoutExtension = Path.GetFileNameWithoutExtension(inputFilePath);
+            var outputFilePath = Path.Combine(fileDirectory, $"{fileWithoutExtension}.pdf");
+
+            using var mappedFileStream = new MappedFile(inputFilePath);
+
+            using var stream = OfficeConverter.ConvertToPDF(mappedFileStream);
+
+            using var outputStream = new FileStream(outputFilePath, FileMode.Create, FileAccess.Write);
+
+            stream.Position = 0;
+            stream.CopyTo(outputStream);
         }
     }
 }
