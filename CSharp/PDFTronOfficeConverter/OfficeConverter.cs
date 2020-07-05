e0fda391802eacb54d9ec4519aa620244c847a99
Jelani Jackson <jeljack86@gmail.com>
2020-07-05 12:01:10 -0500

    Implemented office converter that will convert an office file to PDF

---
+++ CSharp/PDFTronOfficeConverter/OfficeConverter.cs
@@ -0,0 +1,23 @@
+﻿using pdftron.Filters;
+using pdftron.PDF;
+using pdftron.SDF;
+using System.IO;
+
+namespace PDFTronOfficeConverter
+{
+    public static class OfficeConverter
+    {
+        public static Stream ConvertToPDF(MappedFile mappedFile)
+        {
+            using var pdfDoc = new PDFDoc();
+
+            Convert.OfficeToPDF(pdfDoc, mappedFile, null);
+
+            var memoryStream = new MemoryStream();
+
+            pdfDoc.Save(memoryStream, SDFDoc.SaveOptions.e_linearized);
+
+            return memoryStream;
+        }
+    }
+}
