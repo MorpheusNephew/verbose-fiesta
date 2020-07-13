005d10e5ca40132d6f4bfe32814792a5d42c2efa
Jelani Jackson <jeljack86@gmail.com>
2020-07-12 20:10:10 -0500

    Added scripts for create and invoking the lambda function

--- TypeScriptConverter/postbuild.js
+++ TypeScriptConverter/postbuild.js
@@ -1,4 +1,6 @@
 const fs = require('fs-extra');
+const { zip } = require('zip-a-folder');
 
 
-fs.copySync('./node_modules', './build/node_modules/')
+fs.copySync('./node_modules', './build/node_modules/');
+zip('./build', './lambda.zip');
