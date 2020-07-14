8a9abe33b0e9e768135db4c9eda6aeb8cf170323
Jelani Jackson <jeljack86@gmail.com>
2020-07-13 19:35:17 -0500

    Updated postbuild step

--- TypeScriptConverter/postbuild.js
+++ TypeScriptConverter/postbuild.js
@@ -1,6 +1,7 @@
 const fs = require('fs-extra');
 const { zip } = require('zip-a-folder');
 
-
 fs.copySync('./node_modules', './build/node_modules/');
-zip('./build', './lambda.zip');
+fs.copySync('./package.json', './build/package.json');
+fs.copySync('./package-lock.json', './build/package-lock.json');
+zip('./build', './typescript-lambda.zip');
