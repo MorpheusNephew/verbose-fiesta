68de7c99d7cd2f7e227ddc6bcc2e51a1b3673f36
Jelani Jackson <jeljack86@gmail.com>
2020-07-20 21:18:09 -0500

    Removing all things in regards to lambda

--- TypeScriptConverter/postbuild.js
+++ TypeScriptConverter/postbuild.js
@@ -4,4 +4,3 @@ const { zip } = require('zip-a-folder');
 fs.copySync('./node_modules', './build/node_modules/');
 fs.copySync('./package.json', './build/package.json');
 fs.copySync('./package-lock.json', './build/package-lock.json');
-zip('./build', './typescript-lambda.zip');
