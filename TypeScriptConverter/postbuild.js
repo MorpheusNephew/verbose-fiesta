dd5da1bd2a668f1e2ebb88d7db1cdacf581798ca
Jelani Jackson <jeljack86@gmail.com>
2020-07-20 22:33:51 -0500

    Removed a few more unused packages

--- TypeScriptConverter/postbuild.js
+++ TypeScriptConverter/postbuild.js
@@ -1,6 +1,4 @@
 const fs = require('fs-extra');
-const { zip } = require('zip-a-folder');
 
-fs.copySync('./node_modules', './build/node_modules/');
 fs.copySync('./package.json', './build/package.json');
 fs.copySync('./package-lock.json', './build/package-lock.json');
