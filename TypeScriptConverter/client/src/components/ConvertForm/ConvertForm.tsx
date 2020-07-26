d8a9a16e5ca8dd736d0f4ef50b7dae81610e6154
Jelani Jackson <jeljack86@gmail.com>
2020-07-26 13:36:44 -0500

    Created ConvertForm component

---
+++ TypeScriptConverter/client/src/components/ConvertForm/ConvertForm.tsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import path from "path";
+import axios from "axios";
+import {
+  makeStyles,
+  Button,
+  FormControl,
+  Input,
+  InputLabel,
+  FormHelperText,
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  form: {
+    marginTop: theme.spacing(3),
+    display: "flex",
+    flexDirection: "column",
+  },
+}));
+
+const getFileToUpload = (e: any, setFileToUpload: any) => {
+  const file = e.target.files[0];
+  setFileToUpload(file);
+};
+
+const submitFile = async (file?: any) => {
+  if (!file) return;
+
+  const formData = new FormData();
+  formData.append("officeFile", file);
+
+  const response = await axios.post("http://localhost:3030", formData, {
+    responseType: "blob"
+  });
+
+  const convertedPdfName = `${path.basename(
+    file.name,
+    path.extname(file.name)
+  )}.pdf`;
+
+  const url = window.URL.createObjectURL(new Blob([response.data]));
+  const link = document.createElement("a");
+  link.href = url;
+  link.setAttribute("download", convertedPdfName); //or any other extension
+  document.body.appendChild(link);
+  link.click();
+  link.remove();
+};
+
+const ConvertForm = () => {
+  const classes = useStyles();
+
+  const [fileToUpload, setFileToUpload] = useState(null);
+
+  return (
+    <form className={classes.form} action="http://localhost:3000" method="POST">
+      <FormControl>
+        <InputLabel>File to upload:</InputLabel>
+        <Input
+          name="officeFile"
+          type="file"
+          inputProps={{ accept: ".docx,.pptx,.xlsx" }}
+          onChange={(e) => {
+            getFileToUpload(e, setFileToUpload);
+          }}
+        />
+        <FormHelperText></FormHelperText>
+      </FormControl>
+      <Button
+        type="submit"
+        onClick={(e) => {
+          e.preventDefault();
+          submitFile(fileToUpload);
+        }}
+      >
+        Submit
+      </Button>
+    </form>
+  );
+};
+
+export { ConvertForm };
