8d418ee480fbb9fa86183838869b90ab20528246
Jelani Jackson <jeljack86@gmail.com>
2020-07-27 22:36:04 -0500

    Added docker-compose file to build web and api containers

--- TypeScriptConverter/postBuild.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs-extra");
-const path = require('path');
-
-const prependCurrentDir = (filePath) => {
-    return path.join(__dirname, filePath);
-}
-
-fs.copySync(prependCurrentDir('package.json'), prependCurrentDir('build/package.json'));
-fs.copySync(prependCurrentDir('package-lock.json'), prependCurrentDir('build/package-lock.json'));
