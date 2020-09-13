003cccd6abd35e8efd67e785caeba49a40dd8893
Jelani Jackson <jeljack86@gmail.com>
2020-09-13 18:23:23 -0500

    Added routes file to provide routes via a router

---
+++ TypeScriptConverter/api/src/routes.ts
@@ -0,0 +1,27 @@
+import { isNil } from "lodash";
+
+import Router from "@koa/router";
+import Multer from "@koa/multer";
+
+import { convertToPdf } from "./converter";
+
+const router = new Router();
+const storage = Multer.memoryStorage();
+const upload = Multer({ storage: storage });
+
+router.get("/", (ctx) => {
+  ctx.body = "Hello world!!! (koa)";
+});
+
+router.post("/", upload.single("officeFile"), async (ctx) => {
+  if (isNil(ctx.file?.buffer)) {
+    ctx.status = 400;
+    ctx.body = "File with form name officeFile was not found.";
+  } else {
+    const convertedPdfBytes = await convertToPdf(ctx.file.buffer);
+
+    ctx.body = convertedPdfBytes;
+  }
+});
+
+export default router;
